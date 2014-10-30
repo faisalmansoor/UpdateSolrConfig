89213f36667dd1efa70dbfa0be1d927db1b83cfc
Faisal Mansoor <fmansoor@practicefusion.com>
2014-10-30 08:47:47 -0700

    cleanup

--- app.js
+++ app.js
@@ -22,37 +22,35 @@ app.post('/solrdeploy', function (req, res) {
         res.write('Deploying new Solr configuration!\n');
 
         var coreName = req.param('coreName');
-        if (coreName != null && coreName.length == 0) {
+        if (coreName === undefined) {
             res.write('coreName is required.\n');
             printUsage(req, res);
             res.end();
         }
 
-        if (!req.hasOwnProperty('files') && Object.keys(req.files).length === 0) {
+        if (!req.hasOwnProperty('files') || Object.keys(req.files).length === 0) {
             res.write("Config folder zipped with name 'config' is required.\n");
             printUsage(req, res);
             res.end();
         }
 
-        //console.log(req.files);
-
         res.write('stopping solr service.\n');
         var result = shell.exec('/etc/init.d/solr stop');
-        res.write(JSON.stringify(result));
-        res.write('solr service stopping.\n');
+        res.write(JSON.stringify(result) + '\n');
+        res.write('stopped solr service.\n\n');
 
         var configPath = config.solrRoot + coreName + '/conf';
         res.write('deleting existing config files from:' + configPath + '\n');
         result = shell.exec('rm -r ' + configPath);
-        res.write(JSON.stringify(result));
-        res.write('deleted existing config files. \n');
+        res.write(JSON.stringify(result) + '\n' );
+        res.write('deleted existing config files. \n\n');
 
         fs.createReadStream(req.files.config.path).pipe(unzip.Extract({ path: configPath }));
 
         res.write('starting solr service.\n');
         result = shell.exec("/etc/init.d/solr start");
-        res.write(JSON.stringify(result));
-        res.write('solr service started.\n');
+        res.write(JSON.stringify(result) + '\n');
+        res.write('started solr service.\n\n');
 
         res.write('Solr config update!\n');
         res.end();
@@ -65,13 +63,13 @@ app.post('/solrdeploy', function (req, res) {
 
 function printUsage(req, res) {
     var usage = util.format(
-        '\nUSAGE: D:/devTools/Git/bin/curl.exe -i -F "config=@conf.zip" http://%s/solrdeploy?coreName=DocWebSearch\n',
+        '\nUSAGE: D:/devTools/Git/bin/curl.exe -i -F "config=@config.zip" http://%s/solrdeploy?coreName=collection1\n',
         req.headers.host);
 
     res.write(usage);
 };
 
-var server = app.listen(3333, function () {
+var server = app.listen(config.port, function () {
   var host = server.address().address;
   var port = server.address().port;
   console.log('UpdateSolrConfig listening at http://%s:%s', host, port);
--- config.js
+++ config.js
@@ -2,6 +2,7 @@ var config = {};
 
 config.solrRoot = '/opt/solr/solr/';
 config.version = '0.1.0';
+config.port = 3333;
 
 module.exports = config;
 
